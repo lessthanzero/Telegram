e17bbb01cdbfbfbb0f759aee1d786b82ba8ad0a2
Sasha Katin <aleksandr.katin@gmail.com>
2016-05-25 14:00:42 +0300

    add comments to main code, update polling 👓

--- deniro/bot.py
+++ deniro/bot.py
@@ -1,4 +1,9 @@
+# 1. Set encoding like 'charset' in HTML
+
 # -*- coding: utf-8 -*-
+
+# 2. Import external files/classes
+
 import config
 import random
 import requests
@@ -6,17 +11,28 @@ import requests
 import telebot
 from telebot import types
 
+# 3. Set global variables
+
+# 3.1 Set main working instance of TeleBot class
 bot = telebot.TeleBot(config.token)
+
+# 3.2 In this example we're creating an array of random phrases
 phrases = ['You talkin to me?', 'You talkin to me?', 'You talkin to me?', 'Well then who the hell else are you talkin’ to?', 'You talkin’ to me?', 'Well I’m the only one here. Who the fuck do you think you’re talkin’ to?', 'Oh yea? Huh?', 'Okay. Huh?']
+
+# 4. Command handler and photo sending
 
 @bot.message_handler(commands=['start'])
 def send_welcome(message):
     photo = open('tmp/photo.png', 'rb')
     bot.send_photo(message.chat.id, photo)
 
+# 5. Command handler with reply to user
+
 @bot.message_handler(commands=['start'])
 def send_welcome(message):
     bot.reply_to(message, phrases[random.randint(0, 7)])
+
+# 6. Display custom 'keyboard'
 
 @bot.message_handler(commands=['keyboard'])
 def send_welcome(message):
@@ -30,6 +46,8 @@ def send_welcome(message):
     markup.row(itembtnc, itembtnd, itembtne)
     bot.send_message(message.chat.id, "Choose something:", reply_markup=markup)
 
+# 7. Making a request to external API
+
 @bot.message_handler(commands=['request'])
 def make_request(message):
     r = requests.get('https://export.yandex.ru/weather-ng/forecasts/27612.xml')
@@ -37,15 +55,20 @@ def make_request(message):
     #print(r.text)
     bot.send_message(message.chat.id, r.encoding)
 
+# 8. Command handling with a reply message
+
 @bot.message_handler(commands=['help'])
 def send_welcome(message):
     bot.reply_to(message, "What do you want?")
+
+# 9. Handling attachement files sent by user
 
 @bot.message_handler(content_types=['document', 'audio'])
 def handle_docs_audio(message):
     #pass
     bot.reply_to(message, "Why are you showing this to me?")
 
+# 10. Simple conditions in user message parser
 
 @bot.message_handler(content_types=["text"])
 def repeat_all_messages(message):
@@ -57,6 +80,6 @@ def repeat_all_messages(message):
     else:
         bot.reply_to(message, phrases[random.randint(0, 7)])
 
+# Important! Start a bot loop (keep it 'alive')
 
-if __name__ == '__main__':
-     bot.polling(none_stop=True)
+bot.polling()
